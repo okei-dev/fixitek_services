9aa30baefcceb78c4a67275065596d937b630c73
peter-muokwugwo <peter.chiejina@outlook.com>
2025-09-05 15:41:35 -0400

    Update getServices to getServicesByCategory with categoryId

--- frontend/fixitek/src/features/services/serviceApi.tsx
+++ frontend/fixitek/src/features/services/serviceApi.tsx
@@ -1,15 +1,21 @@
 import { api } from "@/lib/api"
-import { ServiceCategory } from "@/types/service";
+import { Category } from "@/types/category";
+import { Service } from "@/types/service";
 
-export const getServices = async () => {
-    const res = await api("/app/services/");
-    console.log(`I am services: ${res.data}`);
+export const getServicesByCategory = async (categoryId: string | number): Promise<Service> => {
+    try {
+        const res = await api(`/app/categories/${categoryId}/services/`);
+        console.log(`I am services: ${res.data}`);
 
-    return res.data;
+        return res.data;
+    } catch (error) {
+        console.error('Failed to fetch services by category', error);
+
+    }
 }
 
 
-export const getServiceCategories = async (): Promise<ServiceCategory[]> => {
+export const getServiceCategories = async (): Promise<Category[]> => {
     const res = await api('/app/categories/');
 
     return res.data;
