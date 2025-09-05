6eaefee78c137a1185306ed784c037cb47e12adf
peter-muokwugwo <peter.chiejina@outlook.com>
2025-09-05 17:51:01 -0400

    Modify  z-index for  mobile menu

--- frontend/fixitek/src/components/Header/Header.tsx
+++ frontend/fixitek/src/components/Header/Header.tsx
@@ -65,7 +65,7 @@ const Header = () => {
             transition: "max-height 0.3s ease-in-out, opacity 0.3s ease-in-out, padding 0.3s ease-in-out",
             overflow: "hidden",
           }}
-          className={`absolute top-28 left-0  right-0  lg:flex gap-6 text-neutral-700 font-medium
+          className={`absolute top-28 left-0 right-0  lg:flex gap-6 text-neutral-700 font-medium z-50
            bg-[var(--neutral--100)] flex flex-col`}>
           {menus.map((menu, idx) => (
             <li key={idx}
