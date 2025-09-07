527b9d0553e19352eac21cb9b6d62551f0749fde
peter-muokwugwo <peter.chiejina@outlook.com>
2025-09-07 17:32:30 -0400

    Create serviceIcons and define its data shape

---
+++ frontend/fixitek/src/lib/serviceIcons.ts
@@ -0,0 +1,16 @@
+export interface Icon {
+    id: number;
+    icon: string;
+}
+
+
+export const serviceIcons: Icon[] = [
+    { id: 1, icon: './img/assembly.png' },
+    { id: 2, icon: './img/wall.png' },
+    { id: 3, icon: './img/smart.png' },
+    { id: 4, icon: './img/plumbing.png' },
+    { id: 5, icon: './img/maintenance.png' },
+    { id: 6, icon: './img/safety.png' },
+    { id: 7, icon: './img/tv.png' },
+    { id: 8, icon: './img/installation.png' }
+];
