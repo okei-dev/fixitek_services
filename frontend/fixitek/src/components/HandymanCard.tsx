39f2af9a2e0af8b5d288f4c75378730e2f906918
peter-muokwugwo <peter.chiejina@outlook.com>
2025-09-08 16:51:27 -0400

    Update HandymanCardProps component

--- frontend/fixitek/src/components/HandymanCard.tsx
+++ frontend/fixitek/src/components/HandymanCard.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { GoArrowRight } from 'react-icons/go'
 
-interface QuoteCardProps {
+interface HandymanCardProps {
     children: React.ReactNode;
 }
 
-const HandymanCard: React.FC<QuoteCardProps> = ({ children }) => {
+const HandymanCard: React.FC<HandymanCardProps> = ({ children }) => {
     return (
         <div className="absolute mx-4 flex flex-col justify-center py-10 px-8 rounded-2xl bg-[var(--accent--primary-1)]">
             <h2 className="text-2xl text-center p-3 text-[var(--neutral--800)]">Contact our expert handyman today!</h2>
