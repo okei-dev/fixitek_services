7c8dc9137157bc1097920432197b8c5a8b57b557
peter-muokwugwo <peter.chiejina@outlook.com>
2025-10-08 15:52:15 -0400

    Built useCategories hook to fetch services according to categories

---
+++ frontend/fixitek/src/hooks/useCategories.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getServiceCategories } from "@/features/services/serviceApi";
+import { Category } from "@/types/category";
+
+export const useCategories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        setLoading(true);
+        const data = await getServiceCategories();
+        setCategories(data);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to fetch categories.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCategories();
+  }, []);
+
+  return { categories, loading, error };
+};
