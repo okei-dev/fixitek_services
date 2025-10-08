aea9a145214e2fbb6064da9ee17fa2037571da3d
peter-muokwugwo <peter.chiejina@outlook.com>
2025-10-08 15:59:27 -0400

    Refactor QuoteCard for reuseability

--- frontend/fixitek/src/components/QuoteCard.tsx
+++ frontend/fixitek/src/components/QuoteCard.tsx
@@ -2,21 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { GoArrowRight } from 'react-icons/go'
 
-interface QuoteCardProps {
-    children: React.ReactNode;
+
+interface Props {
+    title?: string;
+    highlight?: string;
+    ctaText?: string;
+    ctaUrl?: string;
+    children?: React.ReactNode;
+    bgClass?: string;
+    textColor?: string;
 }
 
-const QuoteCard: React.FC<QuoteCardProps> = ({ children }) => {
+
+const QuoteCard: React.FC<Props> = ({
+    title = "Contact our expert handyman today!",
+    highlight,
+    bgClass = "bg-[var(--secondary--color-1)]",
+    textColor = "text-[var(--neutral--100)]",
+    children
+}) => {
     return (
-        <div className="absolute mx-4 -top-20 flex flex-col justify-center py-10 px-8 rounded-2xl bg-[var(--accent--primary-1)]">
-            <h2 className="text-2xl text-center p-3 text-[var(--neutral--800)]">Contact our expert handyman today!</h2>
-            <Link
-                to="/about"
-                className="btn-tertiary-small inline-flex justify-center items-center gap-2"
-            >
-                <p>Get a quote</p>
-                <GoArrowRight />
-            </Link>
+        <div className={`flex flex-col px-6 py-4 rounded-2xl ${bgClass} bg-[url('./img/spiral-blue-line.svg')] bg-cover bg-center`}>
+            <h2 className={`mt-4 p-2 text-3xl ${textColor}`}>
+                {title }{" "}
+                <span className='text-[var(--accent--primary-1)]'>{highlight}</span>
+            </h2>
+            
             {children}
         </div>
     )
