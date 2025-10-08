0b225b7ef1692c42224358bcef8b7a0e74f06d1f
peter-muokwugwo <peter.chiejina@outlook.com>
2025-10-08 15:51:02 -0400

    Update serviceIcons object to include more icons

--- frontend/fixitek/src/lib/serviceIcons.ts
+++ frontend/fixitek/src/lib/serviceIcons.ts
@@ -5,15 +5,15 @@ export interface Icon {
 
 
 export const serviceIcons: Icon[] = [
-    { id: 1, icon: './services/indoor-furniture.png' },
-    { id: 2, icon: './services/outside-furniture.png' },
-    { id: 3, icon: './services/home-security.png' },
-    { id: 4, icon: './services/wall.png' },
-    { id: 5, icon: './services/plumbing.png' },
-    { id: 6, icon: './services/appliances.png' },
-    { id: 7, icon: './services/playground.png' },
-    { id: 8, icon: './services/light.png' },
-    { id: 9, icon: './services/maintenance.png' },
-    { id: 10, icon: './services/gazebo.png' },
-    { id: 11, icon: './services/tv-mount.png' }
+    { id: 1, icon: '/services/indoor-furniture.png' },
+    { id: 2, icon: '/services/outside-furniture.png' },
+    { id: 3, icon: '/services/home-security.png' },
+    { id: 4, icon: '/services/wall.png' },
+    { id: 5, icon: '/services/plumbing.png' },
+    { id: 6, icon: '/services/appliances.png' },
+    { id: 7, icon: '/services/playground.png' },
+    { id: 8, icon: '/services/light.png' },
+    { id: 9, icon: '/services/maintenance.png' },
+    { id: 10, icon: '/services/gazebo.png' },
+    { id: 11, icon: '/services/tv-mount.png' }
 ];
